9112ab6860721be1dd3e44e6762761fb9f0dfe7b
Yana <44026935+yanajulias@users.noreply.github.com>
2023-12-08 08:57:53 +0700

    Create base url and api key into `Constants`

---
+++ app/src/main/java/mini/test/weatherapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package mini.test.weatherapp.util
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    const val API_KEY ="d766408c3dcf512b6aca8bfe9f7d2263"
+}
